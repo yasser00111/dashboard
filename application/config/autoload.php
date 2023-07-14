905db269a7294d1a5e61a166122b32329363fdc0
yasser00111m <yassertkj@gmail.com>
2023-07-15 00:10:21 +0800

    tambah form login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('model_unit1');
+$autoload['model'] = array('model_unit1','model_auth');
